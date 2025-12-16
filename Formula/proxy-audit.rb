04c31d998efd40831b3935c360d05e8c6f945326
mlcbppg <mlcbppgg@163.com>
2025-12-16 10:50:03 +0800

    upgrade proxy tool

--- Formula/proxy-audit.rb
+++ Formula/proxy-audit.rb
@@ -2,9 +2,9 @@ class ProxyAudit < Formula
   desc "macOS CLI tool to audit whether processes are using proxies"
   homepage "https://github.com/m1cbppg/proxy-audit"
   version "0.1.1"
-  url "https://github.com/m1cbppg/proxy-audit/releases/download/v0.1.1/proxy-audit-v0.1.0-aarch64-apple-darwin.tar.gz"
+  url "https://github.com/m1cbppg/proxy-audit/releases/download/v0.1.1/proxy-audit-v0.1.1-aarch64-apple-darwin.tar.gz"
   
-  sha256 "567bfc623a9c5ded07026122422db1c0d8d294736e0954888aa83eb23be3322c"
+  sha256 "6a2bb3da8afa7ef7a4cf649c69752df7548e2f1795c4229c0e87aebcf598774c"
   def install
     bin.install "proxy-audit"
   end
